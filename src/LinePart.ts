57fa3d4adbc1f30192b53b0723c25b5da90782f0
Daniel Ciborowski <dciborow@microsoft.com>
2024-08-17 22:56:00 -0400

    fix: Update LinePart.ts

--- src/LinePart.ts
+++ src/LinePart.ts
@@ -1,12 +1,16 @@
 import { operatorGroups } from "./operatorGroups";
 
+/**
+ * Represents a segment of a line that contains an operator and its metadata.
+ * Used to manage and decorate code segments based on detected operators.
+ */
 export default interface LinePart {
-  text: string;
-  width: number;
-  length: number;
-  operator: string;
-  operatorWidth: number;
-  operatorType: keyof typeof operatorGroups;
-  decorationLocation: number;
-  decoratorChar: string;
+  text: string; // The content of this line part
+  width: number; // Width of the text in pixels
+  length: number; // Length of the text in characters
+  operator: string; // The detected operator in this part
+  operatorWidth: number; // The width of the operator in pixels
+  operatorType: keyof typeof operatorGroups; // The type/category of the operator
+  decorationLocation: number; // Position in the text for decoration
+  decoratorChar: string; // The character used for decoration
 }
